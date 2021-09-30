3c61e56703faacc1636fdf63a58ebb5cdc332936
Rhodri-hc <13622450494@163.com>
2021-09-30 17:46:36 +0800

    小试牛刀

---
+++ cooler/src/components/airConditioner.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="conditioner">
+    <h1 class="conditioner__title">便携小空调</h1>
+    <p class="conditioner__tip">Tip: 为你的夏日带去清凉！</p>
+
+    <div class="conditioner__contain">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.conditioner{
+  color: red;
+}
+
+</style>
